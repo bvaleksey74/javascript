9c3eb63d5dbd4df3afc96a51c2bce31cba5980a1
Aleksey <bvaleksey74@yandex.ru>
2021-03-05 20:43:21 +0500

    hw1, add styles

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+const goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 50
+    },
+    {
+        title: 'Jacket',
+        price: 350
+    },
+    {
+        title: 'Shoes',
+        price: 250
+    },
+];
+const $goodsList = document.querySelector('.goods-list');
 
+const renderGoodsItem = ({
+    title,
+    price
+}) => {
+    return `<div class="goods-item"><div class="goods-item-img"></div><h3>${title}</h3><p>${price}</p><button class="goods-item-button">Добавить</button></div>`;
+};
+
+const renderGoodsList = (list = goods) => {
+    let goodsList = list.map(
+        item => renderGoodsItem(item)
+    ).join('\n');
+
+    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+}
+
+renderGoodsList();
