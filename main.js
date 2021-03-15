d4b3501d914996d9ab2e8910a003bb5290119678
Aleksey <bvaleksey74@yandex.ru>
2021-03-15 20:26:20 +0500

    hw3

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-class ApiMock {
+class Api {
     constructor() {
-
+        this.url = '/goods.json'
     }
-
-    fetch() {
-        return [{
-                title: 'Shirt',
-                price: 150
-            },
-            {
-                title: 'Socks',
-                price: 50
-            },
-            {
-                title: 'Jacket',
-                price: 350
-            },
-            {
-                title: 'Shoes',
-                price: 250
-            },
-        ];
+    fetch(error, success) {
+        var xhr;
+      
+        if (window.XMLHttpRequest) {
+          xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { 
+          xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+      
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+              if(xhr.status === 200){
+            success(JSON.parse(xhr.responseText));}
+            else if(xhr.status >400){
+                error();
+            }
+          }
+        }
+      
+        xhr.open('GET', this.url, true);
+        xhr.send();
     }
 }
 
@@ -37,16 +39,22 @@ class GoodsItem {
 
 class GoodsList {
     constructor() {
-        this.api = new ApiMock();
+        this.api = new Api();
         this.$goodsList = document.querySelector('.goods-list');
         this.goods = [];
+        this.api.fetch(this.onFetchError.bind(this), this.onFetchSuccess.bind(this))
     }
 
-    fetchGoods() {
-        this.goods = this.api.fetch().map(({
+    onFetchSuccess(data){
+        this.goods = data.map(({
             title,
             price
         }) => new GoodsItem(title, price));
+        this.render();
+    }
+
+    onFetchError(){
+        this.$goodsList.insertAdjacentHTML('beforeend', "<h3>Произошла ошибка</h3>");
     }
 
     render() {
@@ -55,34 +63,41 @@ class GoodsList {
             this.$goodsList.insertAdjacentHTML('beforeend', good.getHtml());
         })
     }
-    getTotalPrice() {
-        return this.goods.reduce((total, good) => total += good.price, 0);
+
+    addToCart(){
+        cartList.goods.push(goodsList.goods[0]);
     }
 }
 
 
 class CartItem extends GoodsItem {
-    constructor(options) {
-        super(options)
-        this.size = options.size;
-        this.color = options.color;
-        this.discount = options.discount;
-        this.count = options.count;
-    }
-    deleteFromCart(){        
+    deleteFromCart(){
+     cartList.goods.splice(0, 1)           
     }
 }
 
-class Cart extends GoodsList {
+class Cart{
+    constructor(){
+        this.goods = [];
+    }
+    getItemList(){
+        console.log(this.goods);
+    }
     byuAllItems(){        
     }
     clearCart(){
+        this.goods = [];
+    }
+    getTotalPrice() {
+        return this.goods.reduce((total, good) => total += good.price, 0);
     }
 }
 
 const goodsList = new GoodsList();
 
-goodsList.fetchGoods();
-goodsList.render();
+
+const cartItems = new CartItem();
+const cartList = new Cart();
+cartList.clearCart();
 
 
