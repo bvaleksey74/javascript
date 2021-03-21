6a05646279c73d3c5c9b486db5b4a0d423dd51ad
Aleksey <bvaleksey74@yandex.ru>
2021-03-21 19:08:41 +0500

    hw5

--- main.js
+++ main.js
@@ -1,103 +1,106 @@
-class Api {
-    constructor() {
-        this.url = '/goods.json'
+
+const API_URL = '/goods.json';
+
+
+Vue.component('goods-item', { 
+  template: '<div :data-id="id" class="goods-item"><h3>{{ title }}</h3><p>{{ price }}</p></div>',
+  props: ['title', 'price', 'id'] 
+})
+
+Vue.component('cart', {
+  template: `<div class="cart">
+  <button class="cart-button" @click="openCartHandler" type="button">Корзина</button>
+    <div v-if="isVisibleCart" v-on:click="removeHandler">
+      <slot></slot>
+    </div>
+  </div>`,
+  data() {
+    return {
+      isVisibleCart: false
     }
+  },
+  methods: {
+    openCartHandler() {
+      this.isVisibleCart = !this.isVisibleCart;
+      
+
+    },
+
+    removeHandler(e) {
+      this.$emit('remove', e)
+
+    }
+  }
+})
+
+const vue = new Vue({
+  el: "#app",
+  data: {
+    cart: [],
+    goods: [],
+    filtredGoods: [],
+    search: ''
+  },
+  methods: {
+    addToCartHandler(e) {
+      const id = e.target.closest('.goods-item').dataset.id;
+      const good = this.goods.find((item) => item.id == id);
+
+      this.cart.push(good);
+    },
+
+    removeFromCartHandler(e) {
+      const id = e.target.closest('.goods-item').dataset.id;
+      const goodIndex = this.cart.findIndex((item) => item.id == id);
+
+      this.cart.splice(goodIndex - 1, 1);
+    },
+
+    searchHandler() {
+              if(this.search === '') {
+                this.filtredGoods = this.goods;
+              }
+              const regexp = new RegExp(this.search, 'gi');
+              this.filtredGoods = this.goods.filter((good) => regexp.test(good.title));
+    },
+
     fetch(error, success) {
-        var xhr;
-      
-        if (window.XMLHttpRequest) {
-          xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) { 
-          xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-      
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-              if(xhr.status === 200){
-            success(JSON.parse(xhr.responseText));}
-            else if(xhr.status >400){
-                error();
+            let xhr;
+          
+            if (window.XMLHttpRequest) {
+              xhr = new XMLHttpRequest();
+            } else if (window.ActiveXObject) { 
+              xhr = new ActiveXObject("Microsoft.XMLHTTP");
             }
-          }
-        }
-      
-        xhr.open('GET', this.url, true);
-        xhr.send();
+          
+            xhr.onreadystatechange = function () {
+              if (xhr.readyState === 4) {
+                if(xhr.status === 200) {
+                  success(JSON.parse(xhr.responseText));
+                } else if(xhr.status > 400) {
+                  error('все пропало');
+                }
+              }
+            }
+          
+            xhr.open('GET', API_URL, true);
+            xhr.send();
+    },
+
+    fetchPromise() {
+            return new Promise((resolve, reject) => {
+              this.fetch(reject, resolve)
+            }) 
     }
-}
-
-class GoodsItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-
-    getHtml() {
-        return `<div class="goods-item"><div class="goods-item-img"></div><h3>${this.title}</h3><p>${this.price}</p><button class="goods-item-button">Добавить</button></div>`;
-    }
-}
-
-class GoodsList {
-    constructor() {
-        this.api = new Api();
-        this.$goodsList = document.querySelector('.goods-list');
-        this.goods = [];
-        this.api.fetch(this.onFetchError.bind(this), this.onFetchSuccess.bind(this))
-    }
-
-    onFetchSuccess(data){
-        this.goods = data.map(({
-            title,
-            price
-        }) => new GoodsItem(title, price));
-        this.render();
-    }
-
-    onFetchError(){
-        this.$goodsList.insertAdjacentHTML('beforeend', "<h3>Произошла ошибка</h3>");
-    }
-
-    render() {
-        this.$goodsList.textContent = '';
-        this.goods.forEach((good) => {
-            this.$goodsList.insertAdjacentHTML('beforeend', good.getHtml());
-        })
-    }
-
-    addToCart(){
-        cartList.goods.push(goodsList.goods[0]);
-    }
-}
-
-
-class CartItem extends GoodsItem {
-    deleteFromCart(){
-     cartList.goods.splice(0, 1)           
-    }
-}
-
-class Cart{
-    constructor(){
-        this.goods = [];
-    }
-    getItemList(){
-        console.log(this.goods);
-    }
-    byuAllItems(){        
-    }
-    clearCart(){
-        this.goods = [];
-    }
-    getTotalPrice() {
-        return this.goods.reduce((total, good) => total += good.price, 0);
-    }
-}
-
-const goodsList = new GoodsList();
-
-
-const cartItems = new CartItem();
-const cartList = new Cart();
-cartList.clearCart();
-
-
+  },
+  mounted() {
+    this.fetchPromise()
+      .then(data => {
+        this.goods = data;
+        this.filtredGoods = data;
+      })
+      .catch(err => {
+        console.log(err);
+      }) 
+  }
+})
